c94cf92fcbe85a8c4e64a7471e242fede2dcca71
unknown <antu.haks@gmail.com>
2024-05-19 11:13:56 +0600

    Initalizing git

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response } from 'express'
+import cors from 'cors'
+
+const app: Application = express()
+
+// Parsers
+app.use(express.json())
+app.use(cors())
+
+// respond with "hello world" when a GET request is made to the homepage
+
+app.get('/', (req: Request, res: Response) => {
+  const a = 10
+  res.send('hello world' + a)
+})
+
+export default app
